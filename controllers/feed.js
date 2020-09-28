bb933cdc40485bc209eeab375572eac79c8f311b
aashimgarg <18bcs001@smvdu.ac.in>
2020-09-29 04:02:08 +0530

    added the post route

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,4 +1,19 @@
 exports.getPosts = (req, res, next) => {
+ res.status(200).json({
+     posts: [{
+         title:' My First Post',
+         content:' This is my first post'
+     }]
+ })    
+};
+
+exports.createPost = (req, res, next) => {
+    const title = req.body.title;
+    const content = req.body.content;
     
-  next();
+    res.status(201).json({
+      message: 'Post created successfully!',
+      post: { id: new Date().toISOString(), title: title, content: content }
+    });
   };
+  
