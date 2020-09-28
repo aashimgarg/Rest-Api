9f7cf5bc62df84a9eed7bda3796cfe8836c6ec8d
aashimgarg <18bcs001@smvdu.ac.in>
2020-09-29 01:28:13 +0530

    adding the mvc structure and the feed route

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const feedRoutes = require('./routes/feed');
+
 const app = express();
+
+app.use(bodyParser.json());
+
+app.use('/feed', feedRoutes);
 
 app.listen(8080);
