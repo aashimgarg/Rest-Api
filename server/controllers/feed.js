beb99c2bdcefbaab8081eee8236b3727b14ba211
aashimgarg <18bcs001@smvdu.ac.in>
2020-10-03 23:10:25 +0530

    serving static files and error handling

--- server/controllers/feed.js
+++ server/controllers/feed.js
@@ -1,5 +1,7 @@
 const { validationResult } = require('express-validator/check')
 
+const Post = require('../models/post');
+
 exports.getPosts = (req, res, next) => {
  res.status(200).json({
      posts: [{
@@ -27,15 +29,24 @@ const errors = validationResult(req);
 
   const title = req.body.title;
   const content = req.body.content;
-  // Create post in db
-  res.status(201).json({
-    message: 'Post created successfully!',
-    post: {
-      _id: new Date().toISOString(),
-      title: title,
-      content: content,
-      creator: { name: 'Maximilian' },
-      createdAt: new Date()
-    }
+  const post = new Post({
+    title: title,
+    content: content,
+    imageUrl: 'images/n1.jpg',
+    creator: { name: 'aashim' }
   });
+  post
+    .save()
+    .then(result => {
+      res.status(201).json({
+        message: 'Post created successfully!',
+        post: result
+      });
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
--- server/server.js
+++ server/server.js
@@ -1,11 +1,15 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const feedRoutes = require('./routes/feed');
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -13,8 +17,15 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
 });
-
+ 
 app.use('/feed', feedRoutes);
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  res.status(status).json({ message: message });
+});
 
 mongoose
   .connect(
